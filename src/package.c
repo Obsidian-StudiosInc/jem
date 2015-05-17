e2c6d43e35123c4750b219e1e8fec1d9be941343
William L. Thomson Jr. <wlt@obsidian-studios.com>
2015-05-17 01:15:34 -0400

    Fixed bug with previously loaded dep only loading a single jar, with another wanting all.

--- src/package.c
+++ src/package.c
@@ -196,8 +196,31 @@ struct dep *__gjpGetDeps(struct dep *deps,
             printError("Unable to allocate memory to hold dependency name"); // needs to clean up and exit under error, not just print a message
         deps[i].parsed_sub_deps = false;
         ADDJARS:
-        if(!has_jar)
+        if(!has_jar) {
+            if(deps[i].jars) {
+                char **jars = jemPkgGetJarNames(pkg_name);
+                int j;
+                int k;
+                for(j=0;jars[j];j++) {
+                    bool exists = false;
+                    for(k=0;deps[i].jars[k];k++)
+                        if(strcmp(jars[j],deps[i].jars[k])==0)
+                            exists = true;
+                    if(!exists) {
+                        char **tmp_jars = realloc(deps[i].jars,sizeof(char *)*(k+2));
+                        if(!tmp_jars)
+                            printError("Unable to allocate memory to hold all dependency jars");
+                        deps[i].jars = tmp_jars;
+                        deps[i].jars[k+1]= NULL;
+                        asprintf(&(deps[i].jars[k]),"%s",jars[j]);
+                        if(!deps[i].jars[k])
+                            printError("Unable to allocate memory to hold dependency jar");
+                    }
+                }
+                free(jars);
+            }
             continue;
+        }
         int j = 0;
         bool jar_added = false;
         if(deps[i].jars) {
