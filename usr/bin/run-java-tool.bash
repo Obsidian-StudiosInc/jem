cb1a24210c5ef53a9eb98118ed83b28c4faba3e5
William L. Thomson Jr <wlt@o-sinc.com>
2016-11-30 20:31:03 -0500

    usr/bin/run-java-tool.bash: Sycned with Gentoo run-java-tool.bash changes
    1. Added copyright, missing from upstream
    2. Added condition for icedtea javaws
    3. Quoted variables
    4. shellcheck fix
    5. Replaced `which` with `command -v`
    6. Replaced {EUID} with $(id -u)
    7. Added -n to ${DISPLAY} conditional
    8. Replaced `type` with `command -v`

--- usr/bin/run-java-tool.bash
+++ usr/bin/run-java-tool.bash
@@ -1,44 +1,60 @@
+# Copyright 2016 Obsidian-Studios, Inc.
+# Distributed under the terms of the GNU General Public License v3
+#
+# Original work Copyright 1999-2016 Gentoo Foundation
+
 #!/bin/bash
+
+tool=${0##*/}
+
+if [[ "${tool}" = "javaws" ]] && [[ -x "/usr/bin/itweb-javaws" ]]; then
+	exec "/usr/bin/itweb-javaws" "${@}"
+fi
 
 user_vm="${HOME}/.java/vm"
 system_vm="/etc/jem/vm"
+
 # Try JEM_VM
-if [[ -n ${JEM_VM} ]]; then
-	vmpath="/usr/lib/jvm/${JEM_VM}/"
+if [[ -n "${JEM_VM}" ]]; then
+	vmpath="/usr/lib/jvm/${JEM_VM}"
 # Then user VM
-elif [[ -h ${user_vm} ]]; then
-	vmpath=$(readlink ${user_vm})
+elif [[ -h "${user_vm}" ]]; then
+	vmpath=$(readlink "${user_vm}")
 # And fall back to the system VM
 else
-	vmpath=$(readlink ${system_vm})
+	vmpath=$(readlink "${system_vm}")
 fi
-tool=$(basename $0)
-bin=${vmpath}/bin/${tool}
-jrebin=${vmpath}/jre/bin/${tool}
 
-vm_handle=$(basename ${vmpath})
-if [[ -x ${bin} ]]; then 
-	exec ${bin} "${@}"
-elif [[ -x ${jrebin} ]]; then
-	exec ${jrebin} "${@}"
+vm_handle=${vmpath##*/}
+
+toolpath=$(
+	export PATH=
+        # shellcheck disable=SC1090
+	. "/etc/jem/vms.d/${vm_handle}" 2> /dev/null
+	: ${PATH:=${vmpath}/bin:${vmpath}/jre/bin}
+	command -v "${tool}" 2> /dev/null
+)
+
+if [[ -x "${toolpath}" ]]; then
+	exec "${toolpath}" "${@}"
 else
-	if [[ ! -d ${vmpath} ]]; then
+	if [[ ! -d "${vmpath}" ]]; then
 		echo "* Home for VM '${vm_handle}' does not exist: ${vmpath}" >&2
-		if [[ -n ${JEM_VM} ]]; then
+		if [[ -n "${JEM_VM}" ]]; then
 			echo "* Invalid value for JEM_VM: ${JEM_VM}"
-		elif [[ -h ${user_vm} ]]; then
+		elif [[ -h "${user_vm}" ]]; then
 			echo "* Invalid User VM: ${vm_handle}" >&2
 		else
 			echo "* Invalid System VM: ${vm_handle}" >&2
 		fi
 	else
-		if [[ ${tool} = "run-java-tool.bash" ]]; then
+		if [[ "${tool}" = "run-java-tool.bash" ]]; then
 			echo "* run-java-tool was invoked directly" >&2
 			echo "* run-java-tool should only be used via symlinks to it" >&2
 		else
 			echo "* ${tool} is not available for ${vm_handle} on $(uname -m)" >&2
 			echo "* IMPORTANT: some Java tools are not available on some VMs on some architectures" >&2
-			if (( ${EUID} != 0 )) && [[ "${DISPLAY}" ]] && type -p notify-send > /dev/null; then
+			if [[ "$(id -u)" != 0 ]] && [[ -n "${DISPLAY}" ]] && command -v notify-send > /dev/null; then
 				notify-send -i java-icon48 "Java Launcher" "<b>${tool}</b> is not available for ${vm_handle}"
 			fi
 		fi
